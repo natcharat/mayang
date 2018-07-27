476fee81484210f17a9095c7f70aac6197cc6f39
natcharat <natcharat_nithakorn@cmu.ac.th>
2018-07-27 13:58:09 +0700

    Complete Timerecord

--- resources/views/timerecord.blade.php
+++ resources/views/timerecord.blade.php
@@ -5,19 +5,6 @@
 <body>
 	<div class="container">
 		<div class="row">
-            <body onload="startTime()">
-                <div class="containner">
-                    <div class="row justify-content-center">
-                        <div class="col-md-10">
-                            
-                                <div class="clock" id="txt">
-
-                                </div>
-                            
-                        </div>
-                    </div>
-                </div>
-            </body>
 
             <script>
                 function startTime() {
@@ -33,92 +20,92 @@
                 function checkTime(i) {
                    if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
                    return i;
-                }
+               }
 
-            </script>            
-            <body onload="startTime()">
-                <div class="container">
-                    <div class="row justify-content-center">
-                        <div class="col-md-10">
-                            <center>
-                                <div class="clock" id="txt">
-
-                                </div>
-                            </center>
-                        </div>
-                    </div>
-                </div>
-                <br>
-            </body>
-        </div>
-        <center><br><br>
+           </script>            
+           <body onload="startTime()">
             <div class="container">
-                <div class="row">
-                    <div class="col-2">
-                    </div>
-                    <div class="col-md-4">
-                        <h3><B>เวลาเข้างาน</B></h3>
-                        @if($time == null)
-                        <h2>00:00:00</h2>
-                        @else
-                        <h2>{{$time->time_in}}</h2>
-                        @endif
+                <div class="row justify-content-center">
+                    <div class="col-md-10">
+                        <center>
+                            <div class="clock" id="txt">
 
-
-                    </div>
-                    <div class="col-md-4">
-                        <h3><B>เวลาออกงาน</B></h3>
-                        @if($time == null)
-                        <h2>00:00:00</h2>
-                        @elseif($time->time_off == '23:59:00')
-                        <h2>00:00:00</h2>
-                        @else
-                        <h2>{{$time->time_off}}</h2>
-                        @endif
-                    </div>
-                    <div class="col-2">
+                            </div>
+                        </center>
                     </div>
                 </div>
             </div>
-
-
-            
-        </center>
-
-
-        <!-- button -->
-        <br><br>
-        <center>
-            @if ($time == null)
-            <a href="{{ url('time_in') }}">
-                <button class="timeBtn" id="time" disabled>เข้างาน</button>
-            </a>
-
-            @elseif ($time->status == 'in')
-            <a href="{{ url('time_off') }}">
-                <button class="timeBtn" id="time" disabled>ออกงาน</button>
-            </a>
-
-            @endif
-        </center>
+            <br>
+        </body>
     </div>
+    <center><br><br>
+        <div class="container">
+            <div class="row">
+                <div class="col-2">
+                </div>
+                <div class="col-md-4">
+                    <h3><B>เวลาเข้างาน</B></h3>
+                    @if($time == null)
+                    <h2>00:00:00</h2>
+                    @else
+                    <h2>{{$time->time_in}}</h2>
+                    @endif
 
-    <script type="text/javascript">
-        var IP = {!! json_encode($ip) !!}
 
-        $.ajax({
-            type: 'GET',
-            url: 'https://json.geoiplookup.io',
-            success: function (res) {
-                console.log(res.ip);
-                $("#ip").text(res.ip);
-                if (IP == res.ip) {
-                    $('#time').attr('disabled', false);
-                }
+                </div>
+                <div class="col-md-4">
+                    <h3><B>เวลาออกงาน</B></h3>
+                    @if($time == null)
+                    <h2>00:00:00</h2>
+                    @elseif($time->time_off == '23:59:00')
+                    <h2>00:00:00</h2>
+                    @else
+                    <h2>{{$time->time_off}}</h2>
+                    @endif
+                </div>
+                <div class="col-2">
+                </div>
+            </div>
+        </div>
 
+
+        
+    </center>
+
+
+    <!-- button -->
+    <br><br>
+    <center>
+        @if ($time == null)
+        <a href="{{ url('time_in') }}">
+            <button class="timeBtn" id="time" disabled>เข้างาน</button>
+        </a>
+
+        @elseif ($time->status == 'in')
+        <a href="{{ url('time_off') }}">
+            <button class="timeBtn" id="time" disabled>ออกงาน</button>
+        </a>
+
+        @endif
+    </center>
+</div>
+
+<script type="text/javascript">
+    var IP = {!! json_encode($ip) !!}
+
+    $.ajax({
+        type: 'GET',
+        url: 'https://json.geoiplookup.io',
+        success: function (res) {
+            console.log(res.ip);
+            $("#ip").text(res.ip);
+            if (IP == res.ip) {
+                $('#time').attr('disabled', false);
             }
+
         }
-    })
+    }
+})
 </script>
 
 </body>
