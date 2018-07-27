1ea8ca9f51a5b9665664a05a6a1291c9458ce8c6
rachatapisalertsakul <rachatatle@gmail.com>
2018-07-27 17:50:57 +0700

    commit

--- resources/views/request/index.blade.php
+++ resources/views/request/index.blade.php
@@ -1,11 +1,15 @@
 @extends('layouts.admin')
 
 @section('content')
-<div class="container">
-    <h1>คำขอลางาน</h1>
-    <hr>
-</div>
 
+
+<div class="container" style="border-style: solid;
+    background-color: orange;
+    border-radius: 30px; border-color: black">
+    <h1 align="center">คำขอลางาน</h1><br>
+    
+</div>
+<hr>
 @foreach($requests as $r)
 <div class="container">
     <div class="row justify-content-center">
@@ -13,9 +17,12 @@
             <div class="card" style="background-color: white">
                 <a href="{{ route('request.detail', $r->id) }}">
                     <div class="card-body">
-                        <font color="green">ชื่อ: {{ $r->name }}</font>
-                        <font>ตำแหน่ง: {{ $r->position }}</font>
-                        <font>ประเภท: {{ $r->type }}</font>
+                       <font size="5.5" color="orange">✪</font> 
+                        <font color="orange" size="4">
+                             ชื่อ: {{ $r->name }}&nbsp&nbsp
+                            ตำแหน่ง: {{ $r->position }}&nbsp&nbsp&nbsp
+                            ประเภท: {{ $r->type }}
+                        </font>
                     </div>
                 </a>
             </div>
